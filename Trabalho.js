0bc248a40b6e939f69377078dd7a2858653f4791
Pedro Cirqueira <pedro.augusto1228@gmail.com>
2023-12-01 23:07:28 -0300

    Update: Modal

--- Trabalho.js
+++ Trabalho.js
@@ -5,7 +5,7 @@ var modal = document.getElementById("modal_1");
 var close_modal = document.getElementsByClassName("close_modal")[0];
 
 open_modal.onclick = function () {
-    modal.style.display = "block";
+    modal.style.display = "flex";
 };
 
 close_modal.onclick = function () {
